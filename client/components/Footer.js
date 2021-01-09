89e42d5b07d56000923a92d107a3305085a89ca2
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2021-01-09 23:55:46 +0100

    footer, header

---
+++ client/components/Footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from '../styles/Footer.module.css'
+import Link from "next/link";
+
+export default function Footer() {
+
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.firstContainer}>
+                    <div className={styles.logo_menu}>
+                        <Link href="/"><img src="/footer_logo.svg" alt="footer logo" className={styles.logo}/></Link>
+                        <Link href="/"><a className={styles.text}>Home</a></Link>
+                        <Link href="/explore"><a className={styles.text}>Explore</a></Link>
+                        <Link href="/create" ><a className={styles.text}>Create</a></Link>
+                        <Link href="/timeline"><a className={styles.text}>Timeline</a></Link>
+                    </div>
+                    <div className={styles.logo_menu}>
+                        <h3 className={styles.subTitle}>Company</h3>
+                        <Link href="/contact"><a className={styles.text}>Contact</a></Link>
+                        <Link href="/about"><a className={styles.text}>About Us</a></Link>
+                        <Link href="/" ><a className={styles.text}>Collaborate</a></Link>
+                        <Link href="/"><a className={styles.text}>Support Us</a></Link>
+                    </div>
+
+                </div>
+
+                <div className={styles.logosBox}>
+                    <Link href="/"><img src="/icons.svg" alt="icons" className={styles.icons}/></Link>
+                    <p className={styles.copyFont}>Copyright © 2020-2021</p>
+                </div>
+
+            </div>
+
+
+        </footer>
+
+    )
+}
---
+++ client/components/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Link from 'next/link'
+import styles from '../styles/Header.module.css'
+import { useState, useEffect } from 'react';
+
+export default function Header() {
+
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const listenScrollEvent = e => {
+        if (window.scrollY > 0) {
+            setIsScrolled(true)
+        } else {
+            setIsScrolled(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', listenScrollEvent);
+    })
+
+    return (
+        <header className={`${styles.header} ${isScrolled ? styles.active : ''}`}>
+            <div className={styles.header__title}>
+                <Link href="/"><img src="/jigmap_logo.svg" alt="logo" className={styles.logo} /></Link>
+            </div>
+
+            <div className={styles.navLinks}>
+                <Link href="/about">
+                    <a className={styles.links}>
+                        About
+                    </a>
+                </Link>
+                <Link href="/#contact">
+                    <a className={styles.links}>
+                        Contact
+                    </a>
+                </Link>
+                <Link href="/explore">
+                    <a className={styles.links}>
+                        Explore
+                    </a>
+                </Link>
+                <Link href="/login">
+                    <a className={styles.links}>
+                        Log In
+                    </a>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+
