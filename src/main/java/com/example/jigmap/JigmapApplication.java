9cd8109447421512d066658130bfc3a31d804f7a
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2021-01-09 22:21:32 +0100

    first commit

---
+++ src/main/java/com/example/jigmap/JigmapApplication.java
@@ -0,0 +1,13 @@
+package com.example.jigmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JigmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jigmap/JigmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jigmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JigmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
